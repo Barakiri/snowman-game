6593d44995efceb8b0feeb6e03f8c8fd8c5a5625
Monish Gupta <monish1478@gmail.com>
2024-08-17 15:16:05 -0700

    Sizer, Hot and Cold Prefabs

---
+++ Snowman Project/Assets/Scripts/Sizer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sizer : MonoBehaviour
+{
+    public float ShrinkingRate = 5f;
+    public float GrowthRate = 10f;
+
+    Rigidbody2D Rigidbody2D;
+    Transform Transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        Transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Hot"))
+        {
+            Transform.localScale -= Vector3.one * ShrinkingRate * Time.deltaTime;
+        }
+        else if (collision.collider.CompareTag("Cold"))
+        {
+            Transform.localScale += Vector3.one * GrowthRate * Time.deltaTime * Mathf.Abs(Rigidbody2D.velocity.x);
+        }
+    }
+}
