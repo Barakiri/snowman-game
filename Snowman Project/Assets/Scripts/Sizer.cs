02d2d6e07914525987a56895cc8adc1b4bed4f2c
sneezi909 <vincenttbid@gmail.com>
2024-08-18 21:13:34 -0700

    Add initial death logic

--- Snowman Project/Assets/Scripts/Sizer.cs
+++ Snowman Project/Assets/Scripts/Sizer.cs
@@ -2,40 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Xml.XPath;
 using UnityEngine;
+using System;
 
 public class Sizer : MonoBehaviour
 {
     public float ShrinkingRate = 5f;
     public float GrowthRate = 10f;
 
-    Rigidbody2D Rigidbody2D;
-    Transform Transform;
+    // Size Checks
+    public float MinSize = 0.5f;
+    public float TrueMinSize = 0.2f;
+    public float DeathDelay = 5f;
+    public float rtimer = 0f;
+
+    Rigidbody2D rigidbody2D;
 
     public float radius = 1;
     // Start is called before the first frame update
     void Start()
     {
-        Rigidbody2D = GetComponent<Rigidbody2D>();
-        Transform = GetComponent<Transform>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        sizeDeathCheck();
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
+        print(collision.collider);
         if (collision.collider.CompareTag("Hot"))
         {
-            Transform.localScale -= Vector3.one * ShrinkingRate * Time.deltaTime;
-            radius += -ShrinkingRate * Time.deltaTime;
+            Vector3 localScale = transform.localScale - (Vector3.one * ShrinkingRate * Time.deltaTime);
+            if (radius <= TrueMinSize)
+                transform.localScale = Vector3.one * TrueMinSize;
+            else
+                transform.localScale = localScale;
         }
         else if (collision.collider.CompareTag("Cold"))
         {
-            Transform.localScale += Vector3.one * GrowthRate * Time.deltaTime * Mathf.Abs(Rigidbody2D.velocity.x);
-            radius += GrowthRate * Time.deltaTime * Mathf.Abs(Rigidbody2D.velocity.x);
+            transform.localScale += Vector3.one * GrowthRate * Time.deltaTime * Mathf.Abs(rigidbody2D.velocity.x);
+        }
+        radius = transform.localScale[0];
+    }
+
+    private void sizeDeathCheck()
+    {
+        // Size based death check
+        if (rtimer >= DeathDelay)
+        {
+            print("death" + radius); // Add death logic here.
+            return;
+        }
+        if (radius > MinSize)
+        {
+            rtimer = 0;
+        }
+        else
+        {
+            rtimer += Time.deltaTime;
         }
     }
 }
